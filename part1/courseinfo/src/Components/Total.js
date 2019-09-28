cf6b4f33ed5376c121c576d1d1bbb97b4ff2c59a
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2019-09-28 23:06:19 +0300

    Part1a

---
+++ part1/courseinfo/src/Components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total = (props) => {
+    return (
+        <p>
+            Number of exercises: {props.totalExerciseNumber}
+        </p>
+    )
+}
+
+export default Total;
